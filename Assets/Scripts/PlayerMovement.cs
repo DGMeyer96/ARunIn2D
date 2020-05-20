ce9fc2b9e0a1e71bb28c8e313a96a67cfe956c5c
Daniel Meyer <dmlegoman96@gmail.com>
2020-05-20 15:34:30 -0700

    Added basic movement controls and basic scene

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool run = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (horizontalMove > 0f)
+        {
+            run = true;
+        }
+        else
+        {
+            run = false;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if(Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if(Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+
+        animator.SetBool("Run", run);
+        animator.SetBool("Jump", jump);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+
+        animator.SetBool("Run", run);
+        animator.SetBool("Jump", jump);
+    }
+}
