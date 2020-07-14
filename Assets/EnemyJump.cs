1ace375911792fa78240d2de35cdf2116b68f203
Ronald Goennier <rgoennier@gmail.com>
2020-07-13 20:22:15 -0700

    Initial AI, first enemy

---
+++ Assets/EnemyJump.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyJump : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float jumpForce = 10;
+    bool jumping = false;
+    bool isGrounded;
+    public EnemyDetection eD;
+
+    void Start()
+    {
+        isGrounded = eD.m_Grounded;
+        rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+    }
+
+    void Update()
+    {
+        isGrounded = eD.m_Grounded;
+        //Debug.Log(isGrounded);
+        if (jumping && !isGrounded)
+        {
+            jumping = false;
+            //isGrounded = true;
+            rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+            //rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            rb.freezeRotation = true;
+            Debug.Log(isGrounded);
+        }
+        else
+        {
+            //rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+            //rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+
+        if(!isGrounded)
+        {
+            rb.constraints = RigidbodyConstraints2D.None;
+            rb.freezeRotation = true;
+            rb.gravityScale = 100;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "EnemyJump")
+        {
+            Debug.Log("Plese Jump please");
+            jumping = true;
+            isGrounded = false;
+            rb.constraints = RigidbodyConstraints2D.None;
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        Debug.Log("I am jumping");
+        rb.constraints = RigidbodyConstraints2D.None;
+        rb.AddForce(Vector2.up * jumpForce);
+    }
+}
---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyMove : MonoBehaviour
+{
+    Animator anim;
+    public AIPath aiPath;
+
+    Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(aiPath.desiredVelocity.x >= 1f || aiPath.desiredVelocity.x <= -1f)
+        {
+            
+            anim.SetBool("IsMoving", true);
+        }
+        else
+        {
+            anim.SetBool("IsMoving", false);
+        }
+
+        if (aiPath.desiredVelocity.x >= 0.1f)
+        {
+            transform.localScale = new Vector3(7f, 7f, 7f);
+        }
+        else if (aiPath.desiredVelocity.x <= -0.1f)
+        {
+            transform.localScale = new Vector3(-7f, 7f, 7f);
+        }
+    }
+}
